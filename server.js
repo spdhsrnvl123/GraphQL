859ee3de6bf54b141e9b6d93755aa4ff20e27208
Leutbing <spdhsrnvl123@naver.com>
2022-06-26 19:55:01 +0900

    GraphQL-6

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 import { ApolloServer, gql } from "apollo-server";
 
-#  rest API방식 , graphQl방식
-# GET /text
-# GET /hello
+/* rest API방식 , graphQl방식
+GET /text
+GET /hello
 
-# -동일-
+-동일-
 
-# const typeDefs = gql`
-#   type Query{
-#     text : String
-#     hello : String
-#   }
-# `rest API에서 GET request url을 노출시키는 거와 동일.
-# 
+const typeDefs = gql`
+  type Query{
+    text : String
+    hello : String
+  }
+`rest API에서 GET request url을 노출시키는 거와 동일.
+*/
 
 let tweets = [
   {
@@ -90,13 +90,13 @@ const resolvers = {
       return newTweet;
     },
     deleteTweet(_, { id }) {
-      const tweet = tweets.find((tweet) => tweet.id === id); #먼저 내가 지우려고 하는 tweet을 찾기
-      if (!tweet) return false; #찾기 못했다면 false
-      tweets = tweets.filter((tweet) => tweet.id !== id); #tweet의 id가 삭제하려는 id와 같지 않은 tweet들로 filter를 거치면 tweet.filter는 array를 줄꺼다.
+      const tweet = tweets.find((tweet) => tweet.id === id); //먼저 내가 지우려고 하는 tweet을 찾기
+      if (!tweet) return false; //찾기 못했다면 false
+      tweets = tweets.filter((tweet) => tweet.id !== id);
+      //tweet의 id가 삭제하려는 id와 같지 않은 tweet들로 filter를 거치면 tweet.filter는 array를 줄꺼다.
       return true;
     },
   },
-  
   User: {
     fullName(root) {
       console.log("fullName called!");
